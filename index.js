9119c0990a7a63882e1919ab5023a15dd1103958
abi-wilson25 <agibailw@gmail.com>
2018-07-11 15:34:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 let companyName = 'Scuber'
 function companyNameCheck(){
-  var companyName != 'Scuber';
-  
+  if var companyName != 'Scuber';
+  to.throw(TypeError)
 }
